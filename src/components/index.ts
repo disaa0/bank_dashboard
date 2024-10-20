ddf91f85fbf1d42c6689f58425107fa589ec5a24
AlishKUT <46134296+AlishKUT@users.noreply.github.com>
2024-10-20 08:57:17 +0200

    Account / RecentTransaction

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AccountSummaryWidget } from './AccountSummaryWidget';
 export { default as RecentTransactionsWidget } from './RecentTransactionsWidget';
 export { default as NewsWidget } from './NewsWidget';
+export { default as TopicsWidget } from './TopicsWidget';
 // export { default as CurrencyTrackerWidget } from './CurrencyTrackerWidget';
 // export { default as StockMarketWidget } from './StockMarketWidget';
-export { default as TopicsWidget } from './TopicsWidget';
-
